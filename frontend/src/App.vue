aa85e9f6dc261e02065e8730e5979ba544d8f4fb
박종석 <bruce@bagjongseog-ui-MacBookAir.local>
2019-09-29 16:30:09 +0900

    frontend helloworld version

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full-width center-content">
+    <message-component message="Hello World" />
+  </div>
+</template>
+<script>
+import MessageComponent from './components/MessageComponent.vue'
+export default {
+  components: {
+    MessageComponent
+  }
+}
+</script>
+
